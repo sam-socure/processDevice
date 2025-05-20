f84ca0565dfaab7474f6b4ec64a177c9d4576146
shuang-socure <87031152+shuang-socure@users.noreply.github.com>
2025-05-20 15:37:48 -0700

    added logging

---
+++ processDevice2/Pods/SocureDeviceRisk/DeviceRisk.xcframework/ios-arm64_x86_64-simulator/DeviceRisk.framework/Headers/DeviceRisk.h
@@ -0,0 +1,17 @@
+//
+//  DeviceRisk.h
+//  DeviceRisk
+//
+//  Copyright © 2024 Socure. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <DeviceRisk/SDKInitializer.h>
+
+//! Project version number for DeviceRisk.
+FOUNDATION_EXPORT double DeviceRiskVersionNumber;
+
+//! Project version string for DeviceRisk.
+FOUNDATION_EXPORT const unsigned char DeviceRiskVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeviceRisk/PublicHeader.h>
